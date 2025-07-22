68320ebde840b3a78b550a3f73b26d9297372990
仰望星空 <66815206+sunkangchina@users.noreply.github.com>
2025-07-23 07:51:05 +0800

    Update function.php

--- inc/function.php
+++ inc/function.php
@@ -913,7 +913,7 @@ function get_name($name)
 /**
  * 创建目录
  */
-function create_dir_if_not_exists($arr)
+function create_dir($arr)
 {
     if (is_string($arr)) {
         $v = $arr;
@@ -2599,5 +2599,48 @@ function cache_data($key, $data = null, $expire = 0)
             'expire' => $expire,
             'created_at' => time(),
         ]);
+    }
+}
+
+/**
+ * 解压文件
+ * @param string $input 输入文件, 支持7z, zip, rar, tar, bz2, gz, tar
+ * @param string $output_base 输出目录
+ * @return string
+ */
+function run($input)
+{
+    $output_base = PATH . '/runtime/unzip/';
+    $output_dir = $output_base . md5($input);
+    $ext = get_ext($input);
+    $cmd = "";
+    $tar = "tar -xvf " . $input . " -C " . $output_dir;
+    if (is_dir($output_dir)) {
+        run_cmd("rm -rf " . $output_dir);
+    }
+    create_dir($output_dir);
+    switch ($ext) {
+        case '7z':
+            $cmd = "7za x " . $input . " -o" . $output_dir;
+            break;
+        case 'zip':
+            $cmd = "unzip " . $input . " -d " . $output_dir;
+            break;
+        case 'rar':
+            $cmd = "unar  " . $input . " -o " . $output_dir;
+            break;
+        case 'bz2':
+            $cmd = $tar;
+            break;
+        case 'gz':
+            $cmd = $tar;
+            break;
+        case 'tar':
+            $cmd = $tar;
+            break;
+    }
+    if ($cmd) {
+        run_cmd($cmd);
+        return $output_dir;
     }
 }
